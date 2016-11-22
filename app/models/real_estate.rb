29f5f22465d2b90e2ffd03496c39bf4f90d7c204
kozo yamagata <tune002@gmail.com>
2016-11-22 19:19:06 +0900

    Define validations of RealEstate

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,2 +1,6 @@
 class RealEstate < ApplicationRecord
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :built_at, presence: true
 end
