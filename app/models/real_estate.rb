a954d5e8d3ef75ef26eb9c49ee380ed1a1dba4f5
kozo yamagata <tune002@gmail.com>
2016-11-22 19:21:13 +0900

    Implement RealEstate#age and #age= methods

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -3,4 +3,15 @@ class RealEstate < ApplicationRecord
   validates :rent, presence: true
   validates :address, presence: true
   validates :built_at, presence: true
+
+  def age
+    return 0 if built_at.blank?
+    now = Date.today
+    now.year - built_at.year - (built_at.to_date.change(year: now.year) > now ? 1 : 0)
+  end
+
+  def age=(val)
+    now = Date.today
+    self.built_at = now.change(year: now.year - val).beginning_of_year
+  end
 end
